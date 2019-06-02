6287d589d9d78b0aa6eb2817668eae760c31a66b
align-els <aligng1620@gmail.com>
2019-06-02 16:00:02 +0300

    stopper improvement

--- py-work/stopper.py
+++ py-work/stopper.py
@@ -1,7 +1,7 @@
 import nltk
 
 _stop_words = set(nltk.corpus.stopwords.words("turkish"))
-_stop_words.update(["olan", "bir", "aşağı", "hangi"])
+_stop_words.update(["olan", "bir", "aşağı", "hangi", "şimdi", "şöyle", "böyle"])
 
 def remove_stops(tokens):
     return [token for token in tokens if token not in _stop_words]

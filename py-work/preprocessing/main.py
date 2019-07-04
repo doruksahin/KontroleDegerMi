8669c5491be00716362d21995fcc11009aaf30f1
align-els <aligng1620@gmail.com>
2019-07-04 14:31:04 +0300

    pipeline modified

--- py-work/preprocessing/main.py
+++ py-work/preprocessing/main.py
@@ -1,16 +1,18 @@
 import tr_utils
-import tokenizer, normalizer, stemmer, stopper
+import tokenizer, prenormalizer, normalizer, stemmer, stopper
 
 import locale
 locale.setlocale(locale.LC_ALL,'en_US.UTF-8')
 
 def parse(text):
-    text = normalizer.normalize(text)
+    text = prenormalizer.prenormalize(text)
+
     tokens = tokenizer.tokenize(text)
+    tokens = normalizer.normalize(tokens)
     tokens = stopper.remove_stops(tokens)
     stemmed = stemmer.stem(tokens)
     filtered = stopper.remove_stops(stemmed)
     return filtered
 
-text = "tamam iyi de ama yani şimdi şöyle de bir olay var yani "
+text = "   tamm    iyi de .ama yani; şimdi \"şöyle\" de bir olay var yani "
 print((parse(text)))
--- py-work/preprocessing/normalizer.py
+++ py-work/preprocessing/normalizer.py
@@ -1,10 +1,3 @@
-import string
 
-def lower_case(text):
-    return text.replace("I", 'ı').replace("İ", "i").lower()
-
-def no_punctuation(text):
-    return text.translate(str.maketrans('', '', string.punctuation))
-
-def normalize(text):
-    return no_punctuation(lower_case(text))
+def normalize(tokens):
+    return tokens

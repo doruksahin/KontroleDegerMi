a106e7be766ba9d2ff89ee47b9dc5494e7c35e3e
align-els <aligng1620@gmail.com>
2019-07-16 23:02:50 +0300

    refactring

--- py-work/pipeline/svm_classifier.py
+++ py-work/pipeline/svm_classifier.py
@@ -6,10 +6,19 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVC
 from pprint import pprint
 import json
+import random
+
+# for testing
+with open("test_input.json", "r") as f:
+	test_twits = json.load(f)
 
 # for perfromance measue
-with open("test_input.txt", "r") as f:
-	test_twits = json.load(f)
+with open("expected.json", "r") as f:
+	expected_data = json.load(f)
+
+expected_output = []
+for x in expected_data:
+	expected_output.append(x['is_checkworthy'])
 
 train_data = []
 is_claim = []
@@ -51,30 +60,16 @@ with open("test_feature.csv", "r") as f:
 
 		mini_test_data = [int(f) for f in line[0].split(",")]
 		test_data.append(mini_test_data)
-
+		
 model = SVC(kernel='linear', random_state=0)
-model.fit(train_features, train_labels)
+model.fit(train_data, is_claim)
 predicted = model.predict(test_data)
 
-for i, pred in enumerate(predicted):
-	if pred == 1:
-		print("{}-{}-{}".format(i, pred, test_twits[i]))
+#for i, pred in enumerate(predicted):
+#	if pred == 1:
+#		print("{}-{}-{}".format(i, pred, test_twits[i]))
 
-print(type(predicted))
+#predicted = predicted[0:len(train_data)]
+print(metrics.classification_report(expected_output, predicted))
+print(metrics.confusion_matrix(expected_output, predicted))
 
-
-
-#fiçur normalizatiosn? ve aradğer tahminleri
-'''
-dataset = datasets.load_iris()
-print(dataset)
-print(dataset.data)
-model = GaussianNB()
-model.fit(dataset.data, dataset.target)
-expected = dataset.target
-predicted = model.predict(dataset.data)
-print(predicted)
-
-print(metrics.classification_report(expected, predicted))
-print(metrics.confusion_matrix(expected, predicted))
-'''
